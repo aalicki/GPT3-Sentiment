2b914670ea4a09d22220dfef4bf45867c5f77d41
aalicki <aalicki@gmail.com>
2023-01-14 22:48:50 -0500

    Open Source version of the AI Sentiment Platform.

--- resources/views/sentiment/index.blade.php
+++ resources/views/sentiment/index.blade.php
@@ -1,58 +1,67 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<section class="text-gray-600 body-font relative">
-    @include('sentiment.form')
-</section>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-@if(!empty($sentiment))
-    <section class="text-cyan-600 body-font relative">
-        <div class="lg:w-1/3 bg-sky-700 rounded-lg p-8 flex flex-col mx-auto w-full mt-10 relative z-10 shadow-md">
-            <span class="text-white">Overall Sentiment: <span class="font-bold">{{ strtoupper($sentiment) }}</span></span> <br>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-            <p class="text-white">
-                <span class="font-bold">Text Provided:</span><br>
-                <i>{{$text}}</i>
-            </p>
-        </div>
-    </section>
-@endif
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-<section class="text-gray-600 body-font relative">
-    <div class="lg:w-1/3 bg-gray-300 rounded-lg p-4 flex flex-col mx-auto w-full mt-10 relative z-10 shadow-md">
-        <div x-data="{ expanded: false }">
-            <button @click="expanded = ! expanded">Answers to your Questions &#8595</button>
+<body class="font-sans antialiased bg-cyan-500">
+    <div class="min-h-screen">
 
-            <div x-show="expanded" x-collapse>
-                <p class="mt-5">
-                    <strong>How many times can I check the sentiment of something?</strong><br>
-                    <i>Guests</i> have 3 submissions per minute.<br>
-                    <i>Logged In Users</i> have 10 submissions per minute.<br>
-                    <i>Premium Users</i> have 100 submissions per minute.
-                </p>
+        <section class="text-gray-600 body-font relative">
+            @include('sentiment.form')
+        </section>
 
-                <p class="mt-5">
-                    <strong>Whats the cost of using this?</strong><br>
-                    Currently we're offering this service for free. Though we have limits pending if you're a guest or logged in.
-                    We'll eventually offer a premium service that has higher limits and more features.
-                </p>
+        @if(!empty($sentiment))
+            <section class="text-cyan-600 body-font relative">
+                <div class="lg:w-1/3 bg-sky-700 rounded-lg p-8 flex flex-col mx-auto w-full mt-10 relative z-10 shadow-md">
+                    <span class="text-white">Overall Sentiment: <span class="font-bold">{{ strtoupper($sentiment) }}</span></span> <br>
+
+                    <p class="text-white">
+                        <span class="font-bold">Text Provided:</span><br>
+                        <i>{{$text}}</i>
+                    </p>
+                </div>
+            </section>
+        @endif
+
+        <section class="text-gray-600 body-font relative">
+            <div class="lg:w-1/3 bg-gray-300 rounded-lg p-4 flex flex-col mx-auto w-full mt-10 relative z-10 shadow-md">
+                <div x-data="{ expanded: false }">
+                    <button @click="expanded = ! expanded">Answers to your Questions &#8595</button>
+
+                    <div x-show="expanded" x-collapse>
+                        <p class="mt-5">
+                            <strong>How many times can I check the sentiment of something?</strong><br>
+                            Current limit is <strong>5 /per minute</strong>.<br><br>
+
+                            You can change this in
+                            <pre>
+app/Providers/AppServiceProvider.php
+                        </pre>
+
+                        and set the limit
+                        <pre>
+public $limit = 5;
+                        </pre>
+
+                        to whatever number you want.
+                        </p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
+        </section>
 
-<footer class="text-gray-600 body-font">
-    <div class="bg-cyan-400 mt-20">
-        <div class="lg:w-1/3 container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-            <a class="flex title-font font-medium items-center md:justify-start justify-center text-cyan-900">
-                <img src="{{asset('images/aspira-logo.png')}}" alt="Apsira Sentiment Logo" class="h-16">
-                <span class="ml-3"><strong>v.</strong> 0.1.10</span>
-            </a>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                © 2023 Aspira Sentiment —
-                <a href="https://twitter.com/adamalicki" rel="noopener noreferrer" class="text-gray-600 ml-1 font-bold hover:text-white" target="_blank">@adamalicki</a>
-            </span>
-        </div>
     </div>
-</footer>
+</body>
+</html>
 
-</x-app-layout>
